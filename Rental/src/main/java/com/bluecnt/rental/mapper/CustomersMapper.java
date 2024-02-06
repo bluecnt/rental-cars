a273e5cfeb2916c1923d222d8cb5ff037bd13cd9
YJH9254 <133298353+YJH9254@users.noreply.github.com>
2024-02-06 12:21:44 +0900

    Update CustomersMapper.java
    고객총인원, 페이징처리

--- Rental/src/main/java/com/bluecnt/rental/mapper/CustomersMapper.java
+++ Rental/src/main/java/com/bluecnt/rental/mapper/CustomersMapper.java
@@ -5,9 +5,14 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.bluecnt.rental.dto.CustomerDTO;
+import com.bluecnt.rental.utils.PagingVO;
 
 public interface CustomersMapper {
 
+	// 고객 총 인원
+	public int countCustomer();
+	// 페이징 처리 고객정보 조회
+	public List<CustomerDTO> selectCustomer(PagingVO vo);
 	int addCustomer(CustomerDTO dto); // Create
 	List<CustomerDTO> getAll(); // Read
 	int updateCustomer(@Param("cust_id") int cust_id, @Param("customer") CustomerDTO customer);
