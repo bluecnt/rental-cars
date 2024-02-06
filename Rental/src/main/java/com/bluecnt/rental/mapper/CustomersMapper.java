2f47d2dcc0bd65a6b24a58fa13038041c7c12034
YJH9254 <133298353+YJH9254@users.noreply.github.com>
2024-02-06 23:36:31 +0900

    Update CustomersMapper.java

--- Rental/src/main/java/com/bluecnt/rental/mapper/CustomersMapper.java
+++ Rental/src/main/java/com/bluecnt/rental/mapper/CustomersMapper.java
@@ -13,6 +13,9 @@ public interface CustomersMapper {
 	public int countCustomer();
 	// 페이징 처리 고객정보 조회
 	public List<CustomerDTO> selectCustomer(PagingVO vo);
+	// 카테고리 검색 조회
+	public List<CustomerDTO> searchCustomer(@Param("PagingVO") PagingVO vo,
+			@Param("category") String category, @Param("searchText") String searchText);
 	int addCustomer(CustomerDTO dto); // Create
 	List<CustomerDTO> getAll(); // Read
 	int updateCustomer(@Param("cust_id") int cust_id, @Param("customer") CustomerDTO customer);
