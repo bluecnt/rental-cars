64319e38ff0f4d73e3c6a5981fec0364bac0df4c
YJH9254 <133298353+YJH9254@users.noreply.github.com>
2024-02-06 12:22:28 +0900

    Update CustomersService.java
    service, mapper는 항상 같이 공유함

--- Rental/src/main/java/com/bluecnt/rental/service/CustomersService.java
+++ Rental/src/main/java/com/bluecnt/rental/service/CustomersService.java
@@ -1,11 +1,19 @@
 package com.bluecnt.rental.service;
 
+import java.util.List;
+
 import org.springframework.ui.Model;
 
 import com.bluecnt.rental.dto.CustomerDTO;
+import com.bluecnt.rental.utils.PagingVO;
 
 public interface CustomersService {
 	
+	// 고객 총 인원
+	public int countCustomer();
+	// 페이징 처리 고객정보 조회
+	public List<CustomerDTO> selectCustomer(PagingVO vo);
+	// 고객정보 추가
 	int addCustomer(CustomerDTO dto); // Create 
 	void customersList(Model model); // Read
 	CustomerDTO getCustomerById(int cust_id);
