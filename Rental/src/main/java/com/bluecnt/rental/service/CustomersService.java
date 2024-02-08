616faeca11f5bfb2f0c00b60600aea122eb9012d
YJH9254 <133298353+YJH9254@users.noreply.github.com>
2024-02-08 14:15:13 +0900

    Update CustomersService.java

--- Rental/src/main/java/com/bluecnt/rental/service/CustomersService.java
+++ Rental/src/main/java/com/bluecnt/rental/service/CustomersService.java
@@ -1,9 +1,9 @@
 package com.bluecnt.rental.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.ibatis.annotations.Param;
-import org.springframework.ui.Model;
 
 import com.bluecnt.rental.dto.CustomerDTO;
 import com.bluecnt.rental.utils.PagingVO;
@@ -12,18 +12,18 @@ public interface CustomersService {
 	
 	// 고객 총 인원
 	public int countCustomer();
-	// 페이징 처리 고객정보 조회
+	// 페이징 처리된 고객정보조회
 	public List<CustomerDTO> selectCustomer(PagingVO vo);
-	// 카테고리 검색 조회
+	// 페이징 처리된 카테고리, 검색에 해당하는 고객정보조회
 	public List<CustomerDTO> searchCustomer(@Param("PagingVO") PagingVO vo,
 			@Param("category") String category, @Param("searchText") String searchText);
-	// 고객정보 추가
-	int addCustomer(CustomerDTO dto); // Create 
-	void customersList(Model model); // Read
-	CustomerDTO getCustomerById(int cust_id);
-	int updateCustomer(int cust_id, CustomerDTO dto); // Update
-	int deleteCustomer(int cust_id); // Delete
+	// 고객정보수정 페이지에서 단일고객정보를 보여주는 기능
+	public List<CustomerDTO> getCustomerById(int cust_id);
+	// 고객정보수정 
+	int updateCustomer(CustomerDTO dto);
+	// 고객정보삭제
+	int deleteCustomer(int cust_id); 
+	// 고객정보추가
+	int addCustomer(CustomerDTO dto);
 	
 }
-
-
