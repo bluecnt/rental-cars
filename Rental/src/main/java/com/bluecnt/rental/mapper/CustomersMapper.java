d9ad63b4efe49c600167b220dea8958d22c1c8b4
YJH9254 <133298353+YJH9254@users.noreply.github.com>
2024-02-08 14:14:51 +0900

    Update CustomersMapper.java

--- Rental/src/main/java/com/bluecnt/rental/mapper/CustomersMapper.java
+++ Rental/src/main/java/com/bluecnt/rental/mapper/CustomersMapper.java
@@ -1,6 +1,7 @@
 package com.bluecnt.rental.mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.ibatis.annotations.Param;
 
@@ -11,14 +12,17 @@ public interface CustomersMapper {
 
 	// 고객 총 인원
 	public int countCustomer();
-	// 페이징 처리 고객정보 조회
+	// 페이징 처리된 고객정보조회
 	public List<CustomerDTO> selectCustomer(PagingVO vo);
-	// 카테고리 검색 조회
+	// 페이징 처리된 카테고리, 검색에 해당하는 고객정보조회
 	public List<CustomerDTO> searchCustomer(@Param("PagingVO") PagingVO vo,
 			@Param("category") String category, @Param("searchText") String searchText);
-	int addCustomer(CustomerDTO dto); // Create
-	List<CustomerDTO> getAll(); // Read
-	int updateCustomer(@Param("cust_id") int cust_id, @Param("customer") CustomerDTO customer);
-	CustomerDTO getCustomerById(int cust_id);
-	int deleteCustomer(int cust_id); // Delete
+	// 고객정보수정 페이지에서 단일고객정보를 보여주는 기능
+	public List<CustomerDTO> getCustomerById(int cust_id);
+	// 고객정보수정 
+	void updateCustomer(CustomerDTO dto);
+	// 고객정보삭제
+	void deleteCustomer(int cust_id); 
+	// 고객정보추가
+	int addCustomer(CustomerDTO dto);
 }
